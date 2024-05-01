5c5ac0f481e479dc9be25100c8c2dd4f33e6c1ef
wiedld <wiedld@users.noreply.github.com>
2024-05-01 21:34:06 +0000

    deploy: 6348dc3d0c055a27fe5c111c2cbbbfdadf92ab9d

--- arrow/ffi/sidebar-items.js
+++ arrow/ffi/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["bit_width","create_buffer","from_ffi","from_ffi_and_data_type","to_ffi"],"struct":["FFI_ArrowArray","FFI_ArrowSchema","Flags","ImportedArrowArray"]};
+window.SIDEBAR_ITEMS = {"fn":["export_array_into_raw","from_ffi","from_ffi_and_data_type","to_ffi"],"struct":["FFI_ArrowArray","FFI_ArrowSchema"]};
