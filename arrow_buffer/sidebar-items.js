31c5c5fd79175c87c38da6dc58ca2262d4830882
wiedld <wiedld@users.noreply.github.com>
2024-05-22 15:13:02 +0000

    deploy: 5e9919fd2b66fb987255afac76b9b6d1654676e6

--- arrow_buffer/sidebar-items.js
+++ arrow_buffer/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"mod":["alloc","bigint","bit_chunk_iterator","bit_iterator","bit_mask","bit_util","buffer","builder","bytes","native","util"],"struct":["i256"],"trait":["ArrowNativeType","ToByteSlice"]};
+window.SIDEBAR_ITEMS = {"mod":["alloc","arith","bigint","bit_chunk_iterator","bit_iterator","bit_mask","bit_util","buffer","builder","bytes","interval","native","util"],"struct":["IntervalDayTime","IntervalMonthDayNano","i256"],"trait":["ArrowNativeType","ToByteSlice"]};
